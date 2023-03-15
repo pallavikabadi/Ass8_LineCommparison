cb0aa531cf9d4db366517a573cf4046c4047fbd0
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-03-15 23:39:28 +0530

    added:Check Equality Of Two Lines

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 
 public class LineComparison {
     public static void main(String[] args) {
-        int x1,x2,y1,y2;
-        double lengthofLine;
+        double x1,x2,y1,y2;
+        double x3,x4,y3,y4;
+        double LengthofLine1,LengthofLine2;
         Scanner inputvalues = new Scanner(System.in);
+        //lengthofline1 calculation
         System.out.println("Enter the value of x1 :");
         x1 = inputvalues.nextInt();
         System.out.println("Enter the value of y1 :");
@@ -15,7 +17,27 @@ public class LineComparison {
         x2 = inputvalues.nextInt();
         System.out.println("Enter the value of y2 :");
         y2 = inputvalues.nextInt();
-        lengthofLine = Math.sqrt(Math.pow(x2-x1,2)+ Math.pow(y2-y1,2));
-        System.out.println("The length of a line =" +lengthofLine);
+        LengthofLine1 = Math.sqrt(Math.pow(x2-x1,2)+ Math.pow(y2-y1,2));
+        System.out.println("The length of a line =" +LengthofLine1);
+
+        //lengthofline2 calculation
+        System.out.println("Enter the value of x3 :");
+        x3 = inputvalues.nextInt();
+        System.out.println("Enter the value of y3 :");
+        y3 = inputvalues.nextInt();
+        System.out.println("Enter the value of x4 :");
+        x4 = inputvalues.nextInt();
+        System.out.println("Enter the value of y4 :");
+        y4 = inputvalues.nextInt();
+        LengthofLine2 = Math.sqrt(Math.pow(x4-x3,2)+ Math.pow(y4-y3,2));
+        System.out.println("The length of a line =" +LengthofLine2);
+
+        String Line1=String.valueOf(LengthofLine1);
+        String Line2=String.valueOf(LengthofLine2);
+        if(Line1.equals(Line2))
+            System.out.println("Length of both lines are equal");
+
+        else
+            System.out.println("lengths are not equal");
     }
 }
